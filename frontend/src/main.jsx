0d42ee31803da6a07e7c8b31526a651030374071
ScorTnT <flalxlemdkdlel@naver.com>
2025-02-05 16:33:11 +0900

    [프론트] 가이드라인 작성성

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import GuideLine from './component/GuideLine'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* 해당 위치에 react component 추가 
-      <GuideLine />
+      <GuideLine /> 
     */}
   </StrictMode>,
 )
